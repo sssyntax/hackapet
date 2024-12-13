27c146e03289520aad21031e9995f66712a00e53
acon <101356372+acornitum@users.noreply.github.com>
2024-12-13 12:57:28 -0500

    change hero

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen flex flex-col justify-center items-center">
+        <img src="https://assets.hackclub.com/flag-orpheus-top.svg" className="absolute top-0 left-2"/>
+        <div className="flex flex-col justify-center items-center text-center">
+          <img src="/logo.svg" className="w-full px-6 md:h-44 lg:h-64 xl:h-72" />
+          <p>Code a virtual pet. Get a tamagotchi clone!</p>
+        </div>
+        <img src="/herobg.png" className="absolute bottom-0 w-full"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="bg-darkBlue w-full py-10">
+        <div className="flex flex-wrap justify-center gap-8">
+          <div className="bg-lightBlue p-5 max-w-sm">
+            <p>1. draw and design your character + backgrounds!</p>
+          </div>
+          <div className="bg-lightBlue p-5 max-w-sm">
+            <p>2. program your game - make it move, etc</p>
+          </div>
+          <div className="bg-lightBlue p-5 max-w-sm">
+            <p>3. submit your game - and wait for your package in the mail!</p>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h>fdjsk</h>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
