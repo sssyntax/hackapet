80c36ad25f8b82a6bf409a663b8543c2f340ac8a
acon <101356372+acornitum@users.noreply.github.com>
2024-12-14 14:24:34 -0500

    styling + added section

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -12,8 +12,8 @@ function App() {
         <img src="https://assets.hackclub.com/flag-orpheus-top.svg" className="absolute top-0 left-2"/>
         <div className="flex flex-col justify-center items-center text-center text-[#FFFFFF] mb-24">
           <img src="/logo.svg" className="w-full px-6 md:h-44 lg:h-64 xl:h-72" />
-          <p className="mx-5 text-3xl text-darkPink md:mt-4 mt-8 retro">Code a virtual pet. Get a tamagotchi clone!</p>
-          <button href="#" className="px-8 py-2 bg-lightPink mt-2 text-xl rounded-sm retro">join now !!!!!!</button>
+          <p className="mx-8 text-2xl text-darkPink md:mt-4 mt-8 retro">Code a virtual pet. Get a tamagotchi clone!</p>
+          <button href="#" className="px-8 py-2 bg-lightPink mt-4 text-xl rounded-sm retro">join now !!!!!!</button>
         </div>
         <img src="/herobg.png" className="absolute bottom-0 w-full"/>
       </div>
@@ -31,8 +31,10 @@ function App() {
         </div>
       </div>
 
-      <div>
-        <p>What's in the box?</p>
+      <div className="my-12">
+        <div className="flex flex-col justify-center align-center">
+          <p className="text-center text-3xl retro text-darkBlue">What's in the box?</p>
+        </div>
         <p></p>
       </div>
 
@@ -40,8 +42,10 @@ function App() {
 
       <img src="/footerbg.png" className="w-full"/>
       <div className="bg-black w-full flex flex-col justify-center items-center text-lightBlue pt-4 md:pt-2 pb-6">
-        <p>made by @acon <span className="opacity-70"><i> - follow me on <a href="https://github.com/acornitum" className="text-lightPink" target="_blank" rel="noopener noreferrer">github</a> for a special suprise!</i></span></p>
-        <p>site art by @ItsKareem</p>
+        <div className="text-center mx-5">
+          <p>made by @acon <span className="opacity-70"><i> - follow me on <a href="https://github.com/acornitum" className="text-lightPink" target="_blank" rel="noopener noreferrer">github</a> for a special suprise!</i></span></p>
+          <p>site art by @ItsKareem</p>
+        </div>
       </div>
     </>
   );
