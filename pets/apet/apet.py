bb8914fe6bc2108008d8414aa47a0f59bf7aafaf
acon <101356372+acornitum@users.noreply.github.com>
2025-01-06 13:25:57 -0500

    update game

--- pets/apet/apet.py
+++ pets/apet/apet.py
@@ -2,6 +2,8 @@ import displayio
 from blinka_displayio_pygamedisplay import PyGameDisplay
 import pygame
 import time
+from adafruit_display_text import label
+import random
 
 pygame.init()
 
@@ -13,7 +15,7 @@ forest_background = displayio.OnDiskBitmap("forestbackground.bmp")
 bg_sprite = displayio.TileGrid(forest_background, pixel_shader=forest_background.pixel_shader)
 splash.append(bg_sprite)
 
-cat_sheet = displayio.OnDiskBitmap("cat-Sheet.bmp") #note this isn't actually a bmp file - bmp files don't allow for transparent backgrounds so will need to be edited on actual hackapet circuitpython code
+cat_sheet = displayio.OnDiskBitmap("cat-Sheet.bmp")
 
 tile_width = 32
 tile_height = 32
@@ -27,15 +29,62 @@ cat_sprite = displayio.TileGrid(
     tile_height=tile_height,
     default_tile=0,
     x=(display.width - tile_width) // 2,  
-    y=display.height - tile_height       
+    y=display.height - tile_height - 10     
 )
 
 splash.append(cat_sprite)
 
-frame = 0
-speed = 2  
+fireball_bitmap = displayio.OnDiskBitmap("fireball.bmp")
 
-# gameloop
+fireballs = []
+
+def spawn_fireball():
+    x_position = random.randint(0, display.width - fireball_bitmap.width)
+    fireball = displayio.TileGrid(
+        fireball_bitmap,
+        pixel_shader=fireball_bitmap.pixel_shader,
+        width=1,
+        height=1,
+        tile_width=fireball_bitmap.width,
+        tile_height=fireball_bitmap.height,
+        x=x_position,
+        y=-32
+    )
+    fireballs.append(fireball)
+    splash.append(fireball)
+
+# Function to check for collisions
+def check_collision(sprite1, sprite2):
+    return (
+        sprite1.x < sprite2.x + 32 and
+        sprite1.x + 32 > sprite2.x and
+        sprite1.y < sprite2.y + 32 and
+        sprite1.y + 32 > sprite2.y
+    )
+
+death = displayio.OnDiskBitmap("restart.bmp")
+
+def display_game_over():
+    global death_hi
+    death_hi = displayio.TileGrid(
+        death,
+        pixel_shader=cat_sheet.pixel_shader,
+        width=1,
+        height=1,
+        tile_width=64,
+        tile_height=32,
+        default_tile=0,
+        x=(display.width - 64) // 2,  
+        y=(display.height - 32) // 2  
+    )
+    splash.append(death_hi)
+    for i in fireballs:
+        splash.remove(i)
+    fireballs.clear()
+
+frame = 0
+speed = 4 
+game_over = False
 
 while True:
 
@@ -44,10 +93,32 @@ while True:
             pygame.quit()
             exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                cat_sprite.x -= speed
-            elif event.key == pygame.K_RIGHT:
-                cat_sprite.x += speed
+            if event.key == pygame.K_UP and game_over == True:
+                for i in fireballs:
+                    splash.remove(i)
+                fireballs.clear()
+                splash.remove(death_hi)
+                game_over = False
+
+
+    keys = pygame.key.get_pressed()
+
+    if game_over == False:
+        if keys[pygame.K_LEFT]:
+            cat_sprite.x -= speed
+        if keys[pygame.K_RIGHT]:
+            cat_sprite.x += speed
+        if random.random() < 0.05:  # spawn rate
+            spawn_fireball()
+
+    for fireball in fireballs:
+        fireball.y += 5 
+        if fireball.y > display.height:
+            splash.remove(fireball)
+            fireballs.remove(fireball)
+        elif check_collision(cat_sprite, fireball):
+            game_over = True
+            display_game_over()
 
     cat_sprite[0] = frame
     frame = (frame + 1) % (cat_sheet.width // tile_width)
